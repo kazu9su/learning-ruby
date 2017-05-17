64fed2049b2128ae8c452fac59ac1f248395c480
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-17 10:43:52 +0000

    up

---
+++ 1-8.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# -*- coding:utf-8 -*-
+
+class Duration
+  %w[ days hours minutes seconds ].each do |name|
+    attr_accessor name
+  end
+end
+
+d = Duration.new
+d.days = 3
+d.hours = 5
+p d.days
